8af6267f1a9cbcd5e7b23363b23b26221193b032
m.alramaly <96423919+alramaly22@users.noreply.github.com>
2025-04-20 14:20:58 +0200

    Add files via upload

--- store/models.py
+++ store/models.py
@@ -62,14 +62,16 @@ class Product(models.Model):
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=1000, default='', blank=True, null=True)
-    image = CloudinaryField('image', null=True)  # تعديل هنا
+    image = CloudinaryField('image', null=True)
     is_sale = models.BooleanField(default=False)
     sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     sizes = models.CharField(max_length=100, blank=True, null=True)
     type = models.CharField(max_length=100, default='default_type')
+    created_at = models.DateTimeField(auto_now_add=True)  # ← أضف ده
 
     def __str__(self):
         return self.name
+
 
 # الصور المرتبطة بالمنتجات
 class ProductImage(models.Model):
--- store/views.py
+++ store/views.py
@@ -126,7 +126,7 @@ def product(request, pk):
 
 
 def home(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-created_at')
     featured_images = FeaturedImage.objects.all()
     liked_images = request.user.liked_images.values_list('id', flat=True) if request.user.is_authenticated else []
 
